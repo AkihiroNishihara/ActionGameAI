1020736b7db0d69398d19bc8177871a389dd1a02
光洋 西原 <ocean90light@gmail.com>
2020-08-24 18:20:17 +0900

    add the time remain in gamestage fix the behavior of arrow key for long-press key

--- project/mode_gamestage.py
+++ project/mode_gamestage.py
@@ -4,11 +4,14 @@ import sys
 import itertools
 from project import header as h
 
+TIME_STAGE = 60
+
 
 def game_stage(screen):
-    size_font = 80
+    size_font = 60
     mode_next = "END"
     is_loop = True
+    time_start = pygame.time.get_ticks()
     # フォントの定義
     font = pygame.font.Font(None, size_font)
     # ステージ情報のロード(0:空，1:ブロック，2:player，3:ゴール)
@@ -23,9 +26,15 @@ def game_stage(screen):
 
     while (is_loop):
         pygame.display.update()  # 画面の更新
+
         pygame.time.wait(30)  # 更新時間間隔
         screen.fill((200, 255, 255))
         pygame.draw.rect(screen, (200, 200, 200), Rect(0, 0, h.SCREEN_WIDTH, h.SIZE_BLOCK))  # 画面上部の四角形の描写
+
+        # 経過時間の描写
+        time_elapsed = int((pygame.time.get_ticks() - time_start) / 1000)
+        text_time_remain = font.render(str(TIME_STAGE - time_elapsed), True, (255, 0, 0))  # テキストの作成
+        screen.blit(text_time_remain, [0, 0])  # テキストの描写
 
         # ブロックの描写
         for i, j in itertools.product(range(1, int(h.SCREEN_HEIGHT / h.SIZE_BLOCK)),
@@ -34,27 +43,30 @@ def game_stage(screen):
                 screen.blit(img_brick, [h.SIZE_BLOCK * j, h.SIZE_BLOCK * i])
             elif list_info_stage[i][j] == 2:
                 screen.blit(img_goal, [h.SIZE_BLOCK * j, h.SIZE_BLOCK * i])
-            screen.blit(img_player, rect_player)
+        # プレイヤー描写
+        # 移動処理(長押し)
+        key_pressed = pygame.key.get_pressed()
+        if key_pressed[K_LEFT]:
+            rect_player.move_ip(-1, 0)
+        if key_pressed[K_RIGHT]:
+            rect_player.move_ip(1, 0)
+        if key_pressed[K_DOWN]:
+            rect_player.move_ip(0, 1)
+        if key_pressed[K_UP]:
+            rect_player.move_ip(0, -1)
+        screen.blit(img_player, rect_player)
 
         # イベント処理
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
-            elif event.type == KEYDOWN:
+            if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
                     pygame.quit()
                     sys.exit()
-                if event.key == K_z:
+                elif event.key == K_z:
                     is_loop = False
-                if event.key == K_LEFT:
-                    rect_player.move_ip(-1, 0)
-                if event.key == K_RIGHT:
-                    rect_player.move_ip(1, 0)
-                if event.key == K_UP:
-                    rect_player.move_ip(0, -1)
-                if event.key == K_DOWN:
-                    rect_player.move_ip(0, 1)
     return mode_next
 
 
