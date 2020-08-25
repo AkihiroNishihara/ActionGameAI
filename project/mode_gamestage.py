19e69f6d930580c5af3d2fac59211054a60fd42d
光洋 西原 <ocean90light@gmail.com>
2020-08-25 11:16:36 +0900

    add process of gameover

--- project/mode_gamestage.py
+++ project/mode_gamestage.py
@@ -3,25 +3,30 @@ from pygame.locals import *
 import sys
 import itertools
 from project import header as h
+from project import rectangle
 
-TIME_STAGE = 30
-DIST_BASE_MOVE = 3
+TIME_STAGE = 3
+DIST_BASE_MOVE = 5
 
 
 def game_stage(screen):
     size_font = 60
     mode_next = "END"
     is_loop = True
+    is_game_over = False
     time_start = pygame.time.get_ticks()
     # フォントの定義
     font = pygame.font.Font(None, size_font)
     # ステージ情報のロード(0:空，1:ブロック，2:player，3:ゴール)
     list_info_stage, list_pos_player_ini = load_info_gamestage()
     # 画像のロードおよび情報取得
-    img_player = pygame.image.load("./image/player.png")
+    img_player = pygame.image.load("./image/player.png").convert_alpha()  # 読み込みとピクセル形式の変更（透過あり）
     rect_player = img_player.get_rect()
-    img_brick = pygame.image.load("./image/brick.png")
-    img_goal = pygame.image.load("./image/goal.png")
+    img_brick = pygame.image.load("./image/brick.png").convert()  # 読み込みとピクセル形式の変更
+    img_goal = pygame.image.load("./image/goal.png").convert_alpha()
+    # テキストの設定
+    text_gameover = font.render("Game Over", True, (50, 50, 50))
+    rect_gameover = text_gameover.get_rect()
     # プレイヤーの初期位置更新
     rect_player.move_ip(h.SIZE_BLOCK * list_pos_player_ini[1], h.SIZE_BLOCK * list_pos_player_ini[0])
 
@@ -34,7 +39,8 @@ def game_stage(screen):
 
         # 経過時間の描写
         time_elapsed = int((pygame.time.get_ticks() - time_start) / 1000)
-        text_time_remain = font.render(str(TIME_STAGE - time_elapsed), True, (255, 0, 0))  # テキストの作成
+        time_remain = TIME_STAGE-time_elapsed
+        text_time_remain = font.render(str(max(0, time_remain)), True, (255, 0, 0))  # テキストの作成
         screen.blit(text_time_remain, [0, 0])  # テキストの描写
 
         # ブロックの描写
@@ -46,18 +52,19 @@ def game_stage(screen):
                 screen.blit(img_goal, [h.SIZE_BLOCK * j, h.SIZE_BLOCK * i])
         # プレイヤー描写
         # 移動処理(長押し)
-        key_pressed = pygame.key.get_pressed()
-        if key_pressed[K_LEFT]:
-            rect_player.move_ip(-DIST_BASE_MOVE, 0)
-        if key_pressed[K_RIGHT]:
-            rect_player.move_ip(DIST_BASE_MOVE, 0)
-        if key_pressed[K_DOWN]:
-            rect_player.move_ip(0, DIST_BASE_MOVE)
-        if key_pressed[K_UP]:
-            rect_player.move_ip(0, -DIST_BASE_MOVE)
+        if is_game_over == False:
+            key_pressed = pygame.key.get_pressed()
+            if key_pressed[K_LEFT]:
+                rect_player.move_ip(-DIST_BASE_MOVE, 0)
+            if key_pressed[K_RIGHT]:
+                rect_player.move_ip(DIST_BASE_MOVE, 0)
+            if key_pressed[K_DOWN]:
+                rect_player.move_ip(0, DIST_BASE_MOVE)
+            if key_pressed[K_UP]:
+                rect_player.move_ip(0, -DIST_BASE_MOVE)
         screen.blit(img_player, rect_player)
 
-        # イベント処理
+        # 終了イベント処理
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
@@ -68,8 +75,13 @@ def game_stage(screen):
                     sys.exit()
                 elif event.key == K_z:
                     is_loop = False
-        if TIME_STAGE - time_elapsed < 0:
-            is_loop = False
+
+        # ゲームオーバー処理
+        is_game_over = check_is_gameover(time_remain)
+        if is_game_over == True:
+            screen.blit(text_gameover, rect_gameover)
+            if time_remain < -3:
+                is_loop = False
     return mode_next
 
 
@@ -89,3 +101,10 @@ def load_info_gamestage():
             list_pos_player_ini = [i, j]
 
     return list_info_gamestage, list_pos_player_ini
+
+
+def check_is_gameover(_time_remain):
+    is_gameover = False
+    if _time_remain == 0:
+        is_gameover = True
+    return is_gameover
